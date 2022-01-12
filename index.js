9d37fdaa11e4f7fd1f12f8c55abe503c2837e0f1
Alexander0127 <53129255+Alexander0127@users.noreply.github.com>
2022-01-11 22:10:21 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// boton hamburguesa 
+function Mostrar(){
+    var menu = document.getElementById("menu-mobile");        
+    menu.style.display="block";
+    var btn_menu = document.getElementById("btn-menu"); 
+    btn_menu.style.alignItems="start"
+    var btn_user = document.getElementById("btn-user"); 
+    btn_user.style.display="none";
+    // var header = document.getElementById("header"); 
+    // header.style.background="#F4E6E6"
+}
+function Tema(){
+    var fondo = document.getElementById("body");
+    fondo.style.background = "#000000";
+}
